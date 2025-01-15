3ab3ff0941a9dd6fa8347e7cb7f9fb598d54e692
MyMetric - Marketing Analytics <36673756+mymetric@users.noreply.github.com>
2025-01-15 20:06:40 +0000

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Mudamos")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Agora o endereço oficial é [este aqui](https://hub.mymetric.app/)."
 )
